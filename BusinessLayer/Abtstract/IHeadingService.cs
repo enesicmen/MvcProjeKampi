6f2f3102679febb06ff22c48c39f66a507783100
Enes İcmen <enes_icmen-66@hotmail.com>
2021-10-14 21:44:37 +0300

    Title added. Author data entered.

---
+++ BusinessLayer/Abtstract/IHeadingService.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abtstract
+{
+    public interface IHeadingService
+    {
+        List<Heading> GetCategoryList();
+
+        void HeadingAdd(Heading heading);
+
+        Heading GetById(int Id);
+
+        void HeadingDelete(Heading heading);
+
+        void HeadingUpdate(Heading heading);
+    }
+}

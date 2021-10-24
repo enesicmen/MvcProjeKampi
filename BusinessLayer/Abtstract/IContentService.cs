566488ff9df4148edf23a348048e51c5f8001213
Enes İcmen <enes_icmen-66@hotmail.com>
2021-10-24 19:06:46 +0300

    Text data has been increased according to the title. Listing has been done in accordance with solid principles. Content View section has been edited.

---
+++ BusinessLayer/Abtstract/IContentService.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abtstract
+{
+    public interface IContentService
+    {
+        List<Content> GetCategoryList();
+
+        List<Content> GetListByHeadingId(int Id);
+
+        void ContentAdd(Content content);
+
+        Content GetById(int Id);
+
+        void ContentDelete(Content content);
+
+        void ContentUpdate(Content content);
+    }
+}
---
+++ BusinessLayer/Concrete/ContentManager.cs
@@ -0,0 +1,52 @@
+﻿using BusinessLayer.Abtstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ContentManager : IContentService
+    {
+
+        IContentDal _contentDal;
+
+        public ContentManager(IContentDal contentDal)
+        {
+            _contentDal = contentDal;
+        }
+
+        public void ContentAdd(Content content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ContentDelete(Content content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ContentUpdate(Content content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Content GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Content> GetCategoryList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Content> GetListByHeadingId(int Id)
+        {
+            return _contentDal.List(x=>x.HeadingID==Id);
+        }
+    }
+}
